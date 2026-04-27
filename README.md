dbfa982080e7c71f090c0e13dcc33dbf447ff8ef
Aastha <asthasutariya2@gmail.com>
2026-04-27 17:29:02 +0530

    update

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Chatbots
 
  user want pdf related ans . user upload pdf and ask pdf related question and answer details . with chatbots in Ai python language . user ask chatbots in question answer.
+
+
+
+# command 
+
+- cd (projectname)
+- source venv/bin/activate
+- pip list
+- pip install fastapi uvicorn
+- nano s.py(file name)
+- uvicorn s:app --reload (filename(s))
--- s.py
+++ s.py
@@ -1,17 +1,9 @@
-from fastapi import FastAPI, APIRouter
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
-router = APIRouter()
 
-class Msg(BaseModel):
-    text: str
-
-@router.post("/chat")
-def chat(msg: Msg):
-    if msg.text.lower() == "user":
-        return {"response": "Aastha"}
-    return {"response": "type user"}
-
-
-app.include_router(router)
+@app.get("/chat")
+def chat(text: str):
+    if text.lower() == "user":
+        return {"response": "Astha"}
+    return {"response": "unknown"}
